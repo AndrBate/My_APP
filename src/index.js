706ae2b3d222f8e5226105d99d2afc0be2a39896
André Bate <andrealex548@gmail.com>
2023-05-21 17:46:52 +0100

    adicionando alguns estilos

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { red, deepPurple } from '@mui/material/colors';
 
 import App from './App';
+import './index.css';
+
+
+const theme = createTheme({
+  palette: {
+    primary:{
+      main: red[900],
+    },
+    secondary: {
+      main: deepPurple[900], 
+    }
+  },
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
 
